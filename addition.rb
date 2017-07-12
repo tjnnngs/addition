0fc78633ce5c2b0482d340322f5717c9b7bb7a96
TONYA JENNINGS <tjennings@kcs059702-tl.hitronhub.home>
2017-07-12 11:08:05 -0400

    addition file challenge

--- addition.rb
+++ addition.rb
@@ -1,2 +1,18 @@
-x = 1 + 1
-print "1 + 1 = #{x}"
+def x()
+	x = 1 + 1
+end
+def y()
+	y = 4 + 4
+end
+def z() 
+	z = 10 + 10
+end
+def a()
+	a = x + y + z 
+	end
+
+
+5.times do
+
+p "#{x} + #{y} + #{z} = #{a}"
+end
